42dc05236453184481bd045c972f475e8b60e433
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-02 23:03:15 +0100

    feat: added routes config

---
+++ config/routes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    routes.php
+ * @date    02/01/2025
+ * @time    20:33
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+	// @formatter:off
+
+	// @formatter:on
+};
