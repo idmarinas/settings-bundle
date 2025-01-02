71a3966e15df60779a43a8c47c7a6b6598435883
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-02 23:42:14 +0100

    feat: enhance IdmSettingsBundle with new methods for configuration loading

--- src/IdmSettingsBundle.php
+++ src/IdmSettingsBundle.php
@@ -1,19 +1,37 @@
 <?php
-
 /**
- * This file is part of Bundle "Idm Settings Bundle".
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/settings-bundle/
+ * Last modified by "IDMarinas" on 02/01/2025, 23:36
  *
- * @license https://github.com/idmarinas/settings-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
  *
- * @since 1.0.0
+ * @file    IdmSettingsBundle.php
+ * @date    02/01/2025
+ * @time    20:33
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
  */
 
 namespace Idm\Bundle\Settings;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class IdmSettingsBundle extends Bundle
+final class IdmSettingsBundle extends AbstractBundle
 {
+	public function prependExtension (ContainerConfigurator $container, ContainerBuilder $builder): void
+	{
+		$container->import(dirname(__DIR__) . '/config/cache.php');
+	}
+
+	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+	{
+		$container->import(dirname(__DIR__) . '/config/services.php');
+	}
 }
