f9710cd96d5934ac3004e4f1be4772e13ca812b2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-02 23:40:47 +0100

    feat: added SettingsCacheInterface and SettingsCacheEncryptInterface

---
+++ src/Cache/SettingsCacheEncryptInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/01/2025, 23:40
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    SettingsCacheEncryptInterface.php
+ * @date    02/01/2025
+ * @time    23:14
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Cache;
+
+use Symfony\Contracts\Cache\CacheInterface;
+
+interface SettingsCacheEncryptInterface extends CacheInterface {}
---
+++ src/Cache/SettingsCacheInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/01/2025, 23:22
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    SettingsCacheInterface.php
+ * @date    02/01/2025
+ * @time    23:13
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Cache;
+
+use Symfony\Contracts\Cache\CacheInterface;
+
+interface SettingsCacheInterface extends CacheInterface {}
