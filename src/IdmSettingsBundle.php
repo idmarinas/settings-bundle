0deb36a6d489fc0bbd7abc615bfd40cbfa075733
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-03 22:49:44 +0100

    feat(settings): add cache sodium keypair configuration and validation
    - Implemented configuration for cache sodium keypair in `IdmSettingsBundle`.
    - Added validation method to ensure the keypair is correctly formatted.
    - Updated service loading to include the new configuration parameter.

--- src/IdmSettingsBundle.php
+++ src/IdmSettingsBundle.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 23:36
+ * Last modified by "IDMarinas" on 03/01/2025, 22:49
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -19,12 +19,43 @@
 
 namespace Idm\Bundle\Settings;
 
+use Exception;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class IdmSettingsBundle extends AbstractBundle
 {
+	public function configure (DefinitionConfigurator $definition): void
+	{
+		// @formatter:off
+		$definition->rootNode()
+			->children()
+				->scalarNode('cache_sodium_keypair')
+					->info('key must be generated using sodium_crypto_box_keypair() and encode with base64_encode')
+					->cannotBeOverwritten()
+					->cannotBeEmpty()
+					->defaultValue(
+						base64_decode('A8sxOR+h35ollRFbQ3KzMDp1gBRbn0UZA0kRjclWRChFgNOF9KTcWg1GNV2rHwnT+0xO3h5Kexpq8MSN+mpOBA==')
+					)
+					->validate()
+						->ifString()
+						->then(function ($v) {
+							$key = base64_decode($v, true);
+							$key = is_string($key) ? $key : $v;
+							$valid = $this->sodiumKeypairValid($key);
+
+							return $valid ? $key : null;
+						})
+						->ifNull()->thenInvalid('Invalid cache key')
+					->end()
+				->end()
+			->end()
+		;
+		// @formatter:on
+	}
+
 	public function prependExtension (ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
 		$container->import(dirname(__DIR__) . '/config/cache.php');
@@ -32,6 +63,25 @@ final class IdmSettingsBundle extends AbstractBundle
 
 	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
+		$container->parameters()->set('idm_settings.parameter.cache_keypair', $config['cache_sodium_keypair']);
 		$container->import(dirname(__DIR__) . '/config/services.php');
+	}
+
+	private function sodiumKeypairValid ($keypair): bool
+	{
+		$kpLength = SODIUM_CRYPTO_BOX_KEYPAIRBYTES;
+
+		if (strlen($keypair) !== $kpLength) {
+			return false;
+		}
+
+		try {
+			$publicKey = sodium_crypto_box_publickey($keypair);
+			$secretKey = sodium_crypto_box_secretkey($keypair);
+
+			return !empty($publicKey) && !empty($secretKey);
+		} catch (Exception $e) {
+			return false;
+		}
 	}
 }
