df93bc4dc6aa4ce48bdcd788caf6aebfad5a7fb3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-06 12:19:57 +0100

    fix(security): update security configuration with firewalls and role hierarchy

--- app/config/packages/security.php
+++ app/config/packages/security.php
@@ -2,23 +2,31 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ * Last modified by "IDMarinas" on 06/01/2025, 12:15
  *
  * @project IDMarinas Settings Bundle
- * @see https://github.com/idmarinas/settings-bundle
+ * @see     https://github.com/idmarinas/settings-bundle
  *
- * @file security.php
- * @date 02/01/2025
- * @time 20:33
+ * @file    security.php
+ * @date    02/01/2025
+ * @time    20:33
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since 1.0.0
+ * @since   1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return static function (ContainerConfigurator $container) {
-	$container->extension('security', []);
+	$container->extension('security', [
+		'firewalls'      => [
+			'main' => [],
+		],
+		'role_hierarchy' => [
+			'ROLE_ADMIN'       => 'ROLE_USER',
+			'ROLE_SUPER_ADMIN' => ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH'],
+		],
+	]);
 };
