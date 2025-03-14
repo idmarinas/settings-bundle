89f7eb3463b84acfc4d70e54a9ab137adb4c3098
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 23:47:21 +0100

    style: update formatting and metadata in stof_doctrine_extensions.php

--- app/config/packages/stof_doctrine_extensions.php
+++ app/config/packages/stof_doctrine_extensions.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ * Last modified by "IDMarinas" on 14/03/2025, 23:47
  *
  * @project IDMarinas Settings Bundle
- * @see https://github.com/idmarinas/settings-bundle
+ * @see     https://github.com/idmarinas/settings-bundle
  *
- * @file stof_doctrine_extensions.php
- * @date 02/01/2025
- * @time 20:33
+ * @file    stof_doctrine_extensions.php
+ * @date    14/03/2025
+ * @time    21:52
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since 1.0.0
+ * @since   1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
