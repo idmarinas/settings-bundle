aefe6e5037f38beb78ede6b0aa2175d327f7d6ed
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 23:46:47 +0100

    fix(framework): refactor configuration to use FrameworkConfig class

--- app/config/packages/framework.php
+++ app/config/packages/framework.php
@@ -2,32 +2,33 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ * Last modified by "IDMarinas" on 14/03/2025, 23:46
  *
  * @project IDMarinas Settings Bundle
- * @see https://github.com/idmarinas/settings-bundle
+ * @see     https://github.com/idmarinas/settings-bundle
  *
- * @file framework.php
- * @date 02/01/2025
- * @time 20:33
+ * @file    framework.php
+ * @date    02/01/2025
+ * @time    20:33
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since 1.0.0
+ * @since   1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (ContainerConfigurator $container) {
-	$container->extension('framework', [
-		'secret'                => 'test',
-		'test'                  => true,
-		'http_method_override'  => false,
-		'handle_all_throwables' => true,
-		'php_errors'            => [
-			'log' => true,
-		],
-		'form'                  => false,
-	]);
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $config) {
+	$config
+		->secret('test')
+		->test(true)
+		->httpMethodOverride(false)
+		->handleAllThrowables(true)
+	;
+	$config->form()->enabled(false);
+	$config->propertyAccess()->enabled(true);
+	$config->phpErrors()->log(true);
 };
