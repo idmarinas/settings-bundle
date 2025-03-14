34a3f8d5eb8a72b2fff247db0f3e583f841617bb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 23:44:58 +0100

    refactor(config): rename factories.php and update file paths

--- app/config/factories.php
+++ app/config/factories.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ * Last modified by "IDMarinas" on 14/03/2025, 22:52
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
  *
  * @file    factories.php
- * @date    02/01/2025
- * @time    20:33
+ * @date    14/03/2025
+ * @time    21:52
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.1.0
+ * @since   1.0.0
  */
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -23,7 +23,7 @@ return static function (ContainerConfigurator $container) {
 	// @formatter:off
 	$container
 		->services()
-			->load('Factory\\', dirname(__DIR__) . '/factories')
+			->load('Factory\\', dirname(__DIR__, 2) . '/factories')
 			->public()
 			->autowire()
 			->autoconfigure()
