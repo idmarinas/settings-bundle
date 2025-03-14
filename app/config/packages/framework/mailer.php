55d47a9969ef88b4150c11bdf529fa726e785e89
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 23:48:14 +0100

    fix(mailer): update copyright and metadata information - Adjusted the 'From' header to reflect the correct bundle name.

--- app/config/packages/framework/mailer.php
+++ app/config/packages/framework/mailer.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 02/01/2025, 21:58
+ * Last modified by "IDMarinas" on 14/03/2025, 23:47
  *
  * @project IDMarinas Settings Bundle
- * @see https://github.com/idmarinas/settings-bundle
+ * @see     https://github.com/idmarinas/settings-bundle
  *
- * @file mailer.php
- * @date 02/01/2025
- * @time 20:33
+ * @file    mailer.php
+ * @date    02/01/2025
+ * @time    20:33
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since 1.0.0
+ * @since   1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
@@ -27,7 +27,7 @@ return static function (ContainerConfigurator $container) {
 				'sender' => 'idm_bundle@test.bundle',
 			],
 			'headers'  => [
-				'From' => 'IDMarinas Template Bundle <idm_bundle@test.bundle>',
+				'From' => 'IDMarinas Settings Bundle <idm_bundle@test.bundle>',
 			],
 		],
 	]);
