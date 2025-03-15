67dc30bea81bdc8f135f06f0ce9b8cfc1285ffb8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-15 13:12:33 +0100

    refactor(settings): simplify configuration methods and update imports
    - Removed unused code related to cache sodium keypair.
    - Updated the `loadExtension` method to set the cache keypair parameter correctly.
    - Adjusted the `prependExtension` method to import configuration files based on
      available bundles.
    - Improved code readability by removing commented-out sections.

--- src/IdmSettingsBundle.php
+++ src/IdmSettingsBundle.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 05/01/2025, 19:08
+ * Last modified by "IDMarinas" on 15/03/2025, 13:11
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -19,69 +19,36 @@
 
 namespace Idm\Bundle\Settings;
 
-use Exception;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\UX\TwigComponent\TwigComponentBundle;
 
 final class IdmSettingsBundle extends AbstractBundle
 {
 	public function configure (DefinitionConfigurator $definition): void
 	{
-		// @formatter:off
-		$definition->rootNode()
-			->children()
-				// TODO: convert to an array to add more than 1 key
-				->scalarNode('cache_sodium_keypair')
-					->info("key must be generated using sodium_crypto_box_keypair() and encode with base64_encode.\n"
-					       . 'You need generate your own keypair'
-					)
-					->cannotBeOverwritten()
-					->cannotBeEmpty()
-					->defaultValue(
-						base64_decode('A8sxOR+h35ollRFbQ3KzMDp1gBRbn0UZA0kRjclWRChFgNOF9KTcWg1GNV2rHwnT+0xO3h5Kexpq8MSN+mpOBA==')
-					)
-					->beforeNormalization()
-						->ifString()
-						->then(fn ($v) => base64_decode($v, true))
-					->end()
-					->validate()
-						->ifTrue(fn ($v) => ! $this->sodiumKeypairValid($v))
-						->thenInvalid('Invalid "sodium_crypto_box_keypair"')
-					->end()
-				->end()
-			->end()
-		;
-		// @formatter:on
+		$definition->import(dirname(__DIR__) . '/config/definitions.php');
+	}
+
+	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+	{
+		$builder->setParameter('idm_settings.parameter.cache_keypair', $config['cache_keypair']);
+		$container->import(dirname(__DIR__) . '/config/services.php');
 	}
 
 	public function prependExtension (ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
 		$container->import(dirname(__DIR__) . '/config/cache.php');
-	}
 
-	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-	{
-		$container->parameters()->set('idm_settings.parameter.cache_keypair', $config['cache_sodium_keypair']);
-		$container->import(dirname(__DIR__) . '/config/services.php');
-	}
-
-	private function sodiumKeypairValid ($keypair): bool
-	{
-		$kpLength = SODIUM_CRYPTO_BOX_KEYPAIRBYTES;
-
-		if (strlen($keypair) !== $kpLength) {
-			return false;
+		if ($builder::willBeAvailable('symfony/twig-bundle', TwigBundle::class, ['twig/twig'])) {
+			$container->import(dirname(__DIR__) . '/config/twig.php');
 		}
 
-		try {
-			$publicKey = sodium_crypto_box_publickey($keypair);
-			$secretKey = sodium_crypto_box_secretkey($keypair);
-
-			return !empty($publicKey) && !empty($secretKey);
-		} catch (Exception) {
-			return false;
+		if ($builder::willBeAvailable('symfony/ux-twig-component', TwigComponentBundle::class, ['symfony/twig-bundle'])) {
+			$container->import(dirname(__DIR__) . '/config/twig_component.php');
 		}
 	}
 }
