281f36c753c7e80cf6fa1ec38a9196cbc915de3e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-15 11:59:01 +0100

    refactor(cache): update cache configuration for improved readability
    - Refactored the cache configuration in `config/cache.php` to enhance
      readability and maintainability.
    - Updated the method calls to use `adapters` and `tags` for cache pools.

--- config/cache.php
+++ config/cache.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 15/03/2025, 24:18
+ * Last modified by "IDMarinas" on 15/03/2025, 11:57
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -22,15 +22,13 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $config): void {
-	$config
-		->cache()
-		->pool('idm_settings.cache', [
-			'adapter' => 'idm_settings.service.cache.adapter.settings',
-			'tags'    => true,
-		])
-		->pool('idm_settings.encrypt.cache', [
-			'adapter' => 'idm_settings.service.cache.adapter.settings.encrypt',
-			'tags'    => true,
-		])
+	// @formatter:off
+	$config->cache()
+		->pool('idm_settings.cache')
+			->adapters(['idm_settings.service.cache_adapter.settings'])
+			->tags('settings')
+		->pool('idm_settings.encrypt.cache')
+			->adapters(['idm_settings.service.cache_adapter.settings.encrypt'])
+			->tags(['settings', 'encrypt'])
 	;
 };
