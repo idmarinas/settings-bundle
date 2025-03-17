193c99251814dd90473bcfd53427cadd4ea2a848
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 15:47:22 +0100

    feat(repository): add AbstractSettingRepository class for settings management

---
+++ src/Model/Repository/AbstractSettingRepository.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 16/03/2025, 18:52
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    AbstractSettingRepository.php
+ * @date    14/03/2025
+ * @time    21:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Model\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Idm\Bundle\Settings\Model\Entity\AbstractSetting;
+
+/**
+ * @extends ServiceEntityRepository<AbstractSetting>
+ */
+abstract class AbstractSettingRepository extends ServiceEntityRepository {}
