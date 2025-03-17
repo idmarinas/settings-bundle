3c6303c4e2866ed5f8e2762427cd18acf078c3b3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 15:45:20 +0100

    feat(entity): add abstract setting entity with properties and methods
    - Introduced `AbstractSetting` class to manage settings.
    - Defined properties: `name`, `domain`, `description`, `type`, `value`,
      and `priorityOrder`.
    - Implemented getter and setter methods for all properties.
    - Added validation constraints for `name`, `description`, and `value`.

---
+++ src/Model/Entity/AbstractSetting.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 17/03/2025, 14:10
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    AbstractSetting.php
+ * @date    14/03/2025
+ * @time    21:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Model\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Idm\Bundle\Common\Traits\Entity\UuidTrait;
+use Idm\Bundle\Settings\Enums\SettingsEnum;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\MappedSuperclass]
+abstract class AbstractSetting
+{
+	use UuidTrait;
+
+	#[ORM\Column(type: Types::STRING)]
+	#[Assert\Length(min: 3, max: 255)]
+	protected ?string $name = null;
+
+	#[ORM\ManyToOne(targetEntity: AbstractSettingDomain::class, inversedBy: 'settings')]
+	#[ORM\JoinColumn(nullable: false)]
+	protected ?AbstractSettingDomain $domain = null;
+
+	#[ORM\Column(type: Types::STRING, length: 1000)]
+	#[Assert\Length(min: 0, max: 1000)]
+	protected string $description = '';
+
+	#[ORM\Column(type: Types::STRING, enumType: SettingsEnum::class)]
+	protected SettingsEnum $type = SettingsEnum::STRING;
+
+	#[ORM\Column(type: Types::STRING, length: 1000)]
+	#[Assert\Length(min: 0, max: 1000)]
+	protected string $value = '';
+
+	#[ORM\Column(type: Types::INTEGER)]
+	protected int $priorityOrder = 0;
+
+	public function __toString (): string
+	{
+		return $this->domain . ') ' . $this->name;
+	}
+
+	public function getName (): string
+	{
+		return $this->name;
+	}
+
+	public function setName (string $name): static
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function getDescription (): string
+	{
+		return $this->description;
+	}
+
+	public function setDescription (string $description): static
+	{
+		$this->description = $description;
+
+		return $this;
+	}
+
+	public function getType (): SettingsEnum
+	{
+		return $this->type;
+	}
+
+	public function setType (SettingsEnum $type): static
+	{
+		$this->type = $type;
+
+		return $this;
+	}
+
+	public function getValue (): string
+	{
+		return $this->value;
+	}
+
+	public function setValue (string $value): static
+	{
+		$this->value = $value;
+
+		return $this;
+	}
+
+	public function getDomain (): ?AbstractSettingDomain
+	{
+		return $this->domain;
+	}
+
+	public function setDomain (?AbstractSettingDomain $domain): static
+	{
+		$this->domain = $domain;
+
+		return $this;
+	}
+
+	public function getPriorityOrder (): int
+	{
+		return $this->priorityOrder;
+	}
+
+	public function setPriorityOrder (int $priorityOrder): void
+	{
+		$this->priorityOrder = $priorityOrder;
+	}
+
+	public function getFormatedValue (): float|bool|int|string
+	{
+		return $this->getType()->format($this->getValue());
+	}
+}
