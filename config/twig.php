3fcff92fe5878d83172859fb8050f0aba557a97f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 16:10:50 +0100

    feat(twig): add SettingsExtension and SettingsRuntime services
    - Added the SettingsExtension service to the Twig configuration.
    - Added the SettingsRuntime service to the Twig configuration.

--- config/twig.php
+++ config/twig.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 15/03/2025, 12:03
+ * Last modified by "IDMarinas" on 17/03/2025, 16:14
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -17,9 +17,18 @@
  * @since   1.0.0
  */
 
+use Idm\Bundle\Settings\Twig\Extension\SettingsExtension;
+use Idm\Bundle\Settings\Twig\Runtime\SettingsRuntime;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return function (ContainerConfigurator $container) {
 	// @formatter:off
+	$container->services()
+		->set('.idm_settings.twig.settings_extension', SettingsExtension::class)
+			->tag('twig.extension')
+
+		->set('.idm_settings.twig.settings_runtime', SettingsRuntime::class)
+			->tag('twig.runtime')
+	;
 	// @formatter::on
 };
