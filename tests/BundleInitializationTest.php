bf58ef9a901364dce01dbdedbf05748c2573ebc3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 23:19:56 +0100

    test: remove unused cache interface assertions from BundleInitializationTest
    - Removed assertions for SettingsCacheInterface and
      SettingsCacheEncryptInterface in the BundleInitializationTest.
    - Added additional assertions for 'idm_settings.cache' and
      'idm_settings.encrypt.cache' in the container's removed IDs.

--- tests/BundleInitializationTest.php
+++ tests/BundleInitializationTest.php
@@ -3,7 +3,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/03/2025, 15:40
+ * Last modified by "IDMarinas" on 17/03/2025, 23:18
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -21,8 +21,6 @@
 namespace Idm\Bundle\Settings\Tests;
 
 use App\Kernel;
-use Idm\Bundle\Settings\Interfaces\Cache\SettingsCacheEncryptInterface;
-use Idm\Bundle\Settings\Interfaces\Cache\SettingsCacheInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 final class BundleInitializationTest extends KernelTestCase
@@ -48,9 +46,9 @@ final class BundleInitializationTest extends KernelTestCase
 		$this->assertArrayHasKey('idm_settings.service.cache_adapter.settings', $container->getRemovedIds());
 		$this->assertArrayHasKey('idm_settings.service.cache_adapter.settings.encrypt', $container->getRemovedIds());
 		$this->assertArrayHasKey('idm_settings.cache.sodium_marshaller', $container->getRemovedIds());
+		$this->assertArrayHasKey('idm_settings.cache', $container->getRemovedIds());
+		$this->assertArrayHasKey('idm_settings.encrypt.cache', $container->getRemovedIds());
 
 		$this->assertTrue($container->getParameterBag()->has('idm_settings.parameter.cache_keypair'));
-		$this->assertTrue($container->has(SettingsCacheInterface::class));
-		$this->assertTrue($container->has(SettingsCacheEncryptInterface::class));
 	}
 }
