6d3c87b88eaeeb56baee3bac47d31618e5604b33
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 15:45:36 +0100

    feat(entity): add AbstractSettingDomain class with properties and methods
    - Introduced the AbstractSettingDomain class as a mapped superclass.
    - Added properties: name, priorityOrder, enabled, and readOnly.
    - Included getter and setter methods for each property.
    - Implemented __toString() method to return the name property.

---
+++ src/Model/Entity/AbstractSettingDomain.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 17/03/2025, 14:10
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    AbstractSettingDomain.php
+ * @date    14/03/2025
+ * @time    21:52
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Model\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Idm\Bundle\Common\Traits\Entity\UuidTrait;
+
+#[ORM\MappedSuperclass()]
+abstract class AbstractSettingDomain
+{
+	use UuidTrait;
+
+	#[ORM\Column(type: Types::STRING, unique: true)]
+	protected string $name = 'default';
+
+	#[ORM\Column(type: Types::INTEGER)]
+	protected int $priorityOrder = 0;
+
+	#[ORM\Column(type: Types::BOOLEAN)]
+	protected bool $enabled = false;
+
+	#[ORM\Column(type: Types::BOOLEAN)]
+	protected bool $readOnly = false;
+
+	public function __toString ()
+	{
+		return $this->name;
+	}
+
+	public function getName (): string
+	{
+		return $this->name;
+	}
+
+	public function setName (string $name): self
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function getPriorityOrder (): int
+	{
+		return $this->priorityOrder;
+	}
+
+	public function setPriorityOrder (int $priorityOrder): self
+	{
+		$this->priorityOrder = $priorityOrder;
+
+		return $this;
+	}
+
+	public function getEnabled (): bool
+	{
+		return $this->enabled;
+	}
+
+	public function setEnabled (bool $enabled): self
+	{
+		$this->enabled = $enabled;
+
+		return $this;
+	}
+
+	public function getReadOnly (): bool
+	{
+		return $this->readOnly;
+	}
+
+	public function setReadOnly (bool $readOnly): self
+	{
+		$this->readOnly = $readOnly;
+
+		return $this;
+	}
+}
