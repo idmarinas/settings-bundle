6d3134956bae637d34005bb33e4b3b8e681a9d7b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-17 15:32:11 +0100

    feat(security): update security configuration with user provider and login
    - Added user provider for IDM users using email as the identifier.
    - Configured main firewall with logout path and form login settings.
    - Updated password hashers settings for enhanced security.
    - Adjusted role hierarchy for admin roles.

--- app/config/packages/security.php
+++ app/config/packages/security.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/01/2025, 12:15
+ * Last modified by "IDMarinas" on 16/03/2025, 23:23
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -19,12 +19,43 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use App\Entity\User\User;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 return static function (ContainerConfigurator $container) {
 	$container->extension('security', [
-		'firewalls'      => [
-			'main' => [],
+		'providers'        => [
+			'idm_user_provider' => [
+				'entity' => [
+					'class'    => User::class,
+					'property' => 'email',
+				],
+			],
 		],
-		'role_hierarchy' => [
+		'firewalls'        => [
+			'main' => [
+				'logout'     => [
+					'path' => '/logout',
+				],
+				'provider'   => 'idm_user_provider',
+				'form_login' => [
+					'login_path'          => 'idm_user_login_web',
+					'check_path'          => 'idm_user_login_web',
+					'enable_csrf'         => true,
+					'form_only'           => true,
+					'default_target_path' => 'idm_user_profile_index',
+				],
+			],
+		],
+		'password_hashers' => [
+			PasswordAuthenticatedUserInterface::class => [
+				'algorithm'   => 'auto',
+				'cost'        => 4,
+				'time_cost'   => 3,
+				'memory_cost' => 10,
+			],
+		],
+		'role_hierarchy'   => [
 			'ROLE_ADMIN'       => 'ROLE_USER',
 			'ROLE_SUPER_ADMIN' => ['ROLE_ADMIN', 'ROLE_ALLOWED_TO_SWITCH'],
 		],
