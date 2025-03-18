cda5ad942c52692f9eb277ba8bd01f65fb57097e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-18 22:10:42 +0100

    refactor(entity-listener): update SettingListener to use TagAwareCacheInterface
    - Replaced SettingsCacheInterface with TagAwareCacheInterface in the
      constructor.
    - Modified the postPersist method to use the cache's get method with
      a closure for setting the item and tagging it appropriately.
    - Updated postUpdate and postRemove methods to only invalidate the
      cache tags related to the setting's cache key.
    - Removed unnecessary domain cache key handling from the invalidate
      methods.

--- src/EntityListener/SettingListener.php
+++ src/EntityListener/SettingListener.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/03/2025, 21:54
+ * Last modified by "IDMarinas" on 18/03/2025, 22:03
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -21,28 +21,31 @@ namespace Idm\Bundle\Settings\EntityListener;
 
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
-use Idm\Bundle\Settings\Interfaces\Cache\SettingsCacheInterface;
+use Idm\Bundle\Settings\Enums\SettingsCacheKeysEnum;
 use Idm\Bundle\Settings\Model\Entity\AbstractSetting;
-use Psr\Cache\CacheException;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Cache\InvalidArgumentException;
-use Symfony\Component\Cache\CacheItem;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
-#[AsEntityListener(event: Events::postPersist, lazy: true, entity: AbstractSetting::class)]
-#[AsEntityListener(event: Events::postUpdate, lazy: true, entity: AbstractSetting::class)]
-#[AsEntityListener(event: Events::postRemove, lazy: true, entity: AbstractSetting::class)]
+#[AsEntityListener(event: Events::postPersist, lazy: true)]
+#[AsEntityListener(event: Events::postUpdate, lazy: true)]
+#[AsEntityListener(event: Events::postRemove, lazy: true)]
 readonly class SettingListener
 {
-	public function __construct (private CacheItemPoolInterface&SettingsCacheInterface $cache) {}
+	public function __construct (private CacheItemPoolInterface&TagAwareCacheInterface $cache) {}
 
 	/**
 	 * @throws InvalidArgumentException
-	 * @throws CacheException
 	 */
 	public function postPersist (AbstractSetting $setting): void
 	{
-		$item = new CacheItem();
-		$item->set($setting)->tag([$setting->getCacheKey(), $setting->getDomain()->getCacheKey()]);
+		$item = $this->cache->get($setting->getCacheKey(), function (ItemInterface $item) use ($setting) {
+			return $item
+				->set($setting)
+				->tag([SettingsCacheKeysEnum::ITEM->value, $setting->getCacheKey(), $setting->getDomain()->getCacheKey()])
+			;
+		});
 
 		$this->cache->save($item);
 	}
@@ -52,8 +55,7 @@ readonly class SettingListener
 	 */
 	public function postUpdate (AbstractSetting $setting): void
 	{
-		$this->cache->invalidateTags([$setting->getCacheKey(), $setting->getDomain()->getCacheKey()]);
-		$this->cache->delete($setting->getCacheKey());
+		$this->cache->invalidateTags([$setting->getCacheKey()]);
 	}
 
 	/**
@@ -61,7 +63,7 @@ readonly class SettingListener
 	 */
 	public function postRemove (AbstractSetting $setting): void
 	{
-		$this->cache->invalidateTags([$setting->getCacheKey(), $setting->getDomain()->getCacheKey()]);
+		$this->cache->invalidateTags([$setting->getCacheKey()]);
 		$this->cache->delete($setting->getCacheKey());
 	}
 }
