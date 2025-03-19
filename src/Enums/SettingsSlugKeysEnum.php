7652194957f3069bef69d414c1639ebea4ba2e6e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-19 21:36:06 +0100

    refactor(enum): rename SettingsCacheKeysEnum to SettingsSlugKeysEnum and add slug method

--- src/Enums/SettingsSlugKeysEnum.php
+++ src/Enums/SettingsSlugKeysEnum.php
@@ -2,12 +2,12 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 18/03/2025, 23:01
+ * Last modified by "IDMarinas" on 19/03/2025, 21:34
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
  *
- * @file    SettingsCacheKeysEnum.php
+ * @file    SettingsSlugKeysEnum.php
  * @date    02/01/2025
  * @time    23:00
  *
@@ -19,10 +19,19 @@
 
 namespace Idm\Bundle\Settings\Enums;
 
-enum SettingsCacheKeysEnum: string
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+enum SettingsSlugKeysEnum: string
 {
 	case COLLECTION_DOMAIN = 'idm.settings.collection.domain';
 	case COLLECTION_ITEMS  = 'idm.settings.collection.items';
 	case DOMAIN_ITEM       = 'idm.settings.domain.item';
 	case ITEM              = 'idm.settings.item';
+
+	public static function slug (string $entityName, string $name): string
+	{
+		$key = $entityName . '.' . $name;
+
+		return ((new AsciiSlugger())->slug($key, '.'));
+	}
 }
