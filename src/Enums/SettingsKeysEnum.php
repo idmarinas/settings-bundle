83124196aaaab51ea39ed62dfe0f4f33fa8eaa81
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-21 00:12:43 +0100

    feat(enum): add COLLECTION_SETTINGS_BY_ENTITY_ID_AND_DOMAIN to SettingsKeysEnum

--- src/Enums/SettingsKeysEnum.php
+++ src/Enums/SettingsKeysEnum.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 20/03/2025, 23:10
+ * Last modified by "IDMarinas" on 21/03/2025, 24:12
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -23,12 +23,13 @@ use Symfony\Component\String\Slugger\AsciiSlugger;
 
 enum SettingsKeysEnum: string
 {
-	case COLLECTION_SETTINGS_BY_DOMAIN    = 'idm.settings.collection.settings.by.domain';
-	case COLLECTION_SETTINGS              = 'idm.settings.collection.settings';
-	case COLLECTION_DOMAINS               = 'idm.settings.collection.domains';
-	case COLLECTION_SETTINGS_BY_ENTITY_ID = 'idm.settings.collection.entity.id';
-	case DOMAIN                           = 'idm.settings.domain';
-	case SETTING                          = 'idm.settings.setting';
+	case COLLECTION_SETTINGS_BY_DOMAIN               = 'idm.settings.collection.settings.by.domain';
+	case COLLECTION_SETTINGS                         = 'idm.settings.collection.settings';
+	case COLLECTION_DOMAINS                          = 'idm.settings.collection.domains';
+	case COLLECTION_SETTINGS_BY_ENTITY_ID            = 'idm.settings.collection.entity.id';
+	case COLLECTION_SETTINGS_BY_ENTITY_ID_AND_DOMAIN = 'idm.settings.collection.entity.id.and.domain';
+	case DOMAIN                                      = 'idm.settings.domain';
+	case SETTING                                     = 'idm.settings.setting';
 
 	public static function slug (string $entityName, string $name): string
 	{
