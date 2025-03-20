df8df2277f2fd545b67963e57bcd3ad4ab2bae98
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-20 18:03:57 +0100

    feat(SettingsCache): add setCache method to interface with CacheItemPoolInterface

--- src/Interfaces/Cache/SettingsCacheInterface.php
+++ src/Interfaces/Cache/SettingsCacheInterface.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/03/2025, 16:50
+ * Last modified by "IDMarinas" on 20/03/2025, 17:20
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -19,6 +19,10 @@
 
 namespace Idm\Bundle\Settings\Interfaces\Cache;
 
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
-interface SettingsCacheInterface extends TagAwareCacheInterface {}
+interface SettingsCacheInterface
+{
+	public function setCache (CacheItemPoolInterface&TagAwareCacheInterface $cache): self;
+}
