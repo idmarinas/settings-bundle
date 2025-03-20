58b68ce82c834413410418476f08a5285383d646
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-20 23:56:14 +0100

    feat(exception): add RequiredFieldMissingException class

---
+++ src/Exception/RequiredFieldMissingException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/03/2025, 23:47
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    RequiredFieldMissingException.php
+ * @date    20/03/2025
+ * @time    23:30
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Exception;
+
+use Exception;
+
+final class RequiredFieldMissingException extends Exception {}
