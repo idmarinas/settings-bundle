d6621613f2dd5b7a71982eda5205485896a020b2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-20 23:55:28 +0100

    test: add initial test for SettingDomainRepository functionality

---
+++ tests/Repository/SettingDomainRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/03/2025, 22:41
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    SettingDomainRepositoryTest.php
+ * @date    20/03/2025
+ * @time    18:31
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Tests\Repository;
+
+use Idm\Bundle\Settings\Model\Entity\AbstractSettingDomain;
+use Idm\Bundle\Settings\Model\Repository\AbstractSettingDomainRepository;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SettingDomainRepositoryTest extends WebTestCase
+{
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	public function testSomething (): void
+	{
+		self::bootKernel();
+
+		$em = static::getContainer()->get('doctrine.orm.entity_manager');
+		/** @var AbstractSettingDomainRepository $repository */
+		$repository = $em->getRepository(AbstractSettingDomain::class);
+
+		$domain = $repository->getDomainObject('not-existing-domain');
+
+		$this->assertNull($domain);
+	}
+}
