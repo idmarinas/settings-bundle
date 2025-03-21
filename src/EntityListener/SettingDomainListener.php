4d7fb7b6107dc6a36988cc45199249f931c4b781
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-21 21:46:46 +0100

    feat(listener): enhance cache handling in SettingDomainListener
    - Added a new dependency for cache encryption.
    - Updated postPersist to use a dynamic cache retrieval method.
    - Modified postUpdate and postRemove to leverage the new cache method.
    - Implemented a private getCache method to determine the appropriate cache.

--- src/EntityListener/SettingDomainListener.php
+++ src/EntityListener/SettingDomainListener.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 20/03/2025, 23:09
+ * Last modified by "IDMarinas" on 21/03/2025, 21:43
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -22,6 +22,7 @@ namespace Idm\Bundle\Settings\EntityListener;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
 use Idm\Bundle\Settings\Enums\SettingsKeysEnum;
+use Idm\Bundle\Settings\Interfaces\Entity\UseEncryptCacheInterface;
 use Idm\Bundle\Settings\Model\Entity\AbstractSettingDomain;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Cache\InvalidArgumentException;
@@ -33,21 +34,24 @@ use Symfony\Contracts\Cache\TagAwareCacheInterface;
 #[AsEntityListener(event: Events::postRemove, lazy: true)]
 readonly class SettingDomainListener
 {
-	public function __construct (private CacheItemPoolInterface&TagAwareCacheInterface $cache) {}
+	public function __construct (
+		private CacheItemPoolInterface&TagAwareCacheInterface $cache,
+		private CacheItemPoolInterface&TagAwareCacheInterface $cacheEncrypt
+	) {}
 
 	/**
 	 * @throws InvalidArgumentException
 	 */
 	public function postPersist (AbstractSettingDomain $domain): void
 	{
-		$item = $this->cache->get($domain->getSlug(), function (ItemInterface $item) use ($domain) {
-			return $item
-				->set($domain)
-				->tag([SettingsKeysEnum::DOMAIN->value, $domain->getSlug()])
-			;
-		});
+		$this->getCache($domain::class)->get(
+			$domain->getSlug(),
+			function (ItemInterface $item) use ($domain) {
+				$item->tag([SettingsKeysEnum::DOMAIN->value, $domain->getSlug()]);
 
-		$this->cache->save($item);
+				return $domain;
+			}
+		);
 	}
 
 	/**
@@ -55,7 +59,7 @@ readonly class SettingDomainListener
 	 */
 	public function postUpdate (AbstractSettingDomain $domain): void
 	{
-		$this->cache->invalidateTags([$domain->getSlug()]);
+		$this->getCache($domain::class)->invalidateTags([$domain->getSlug()]);
 	}
 
 	/**
@@ -63,7 +67,12 @@ readonly class SettingDomainListener
 	 */
 	public function postRemove (AbstractSettingDomain $domain): void
 	{
-		$this->cache->invalidateTags([$domain->getSlug()]);
-		$this->cache->delete($domain->getSlug());
+		$this->getCache($domain::class)->invalidateTags([$domain->getSlug()]);
+		$this->getCache($domain::class)->delete($domain->getSlug());
+	}
+
+	private function getCache (string $class): CacheItemPoolInterface&TagAwareCacheInterface
+	{
+		return is_subclass_of($class, UseEncryptCacheInterface::class) ? $this->cacheEncrypt : $this->cache;
 	}
 }
