dd6c1983fd55386b42ebd3566f617f86263a03e0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-21 21:47:12 +0100

    feat(trait): add EncryptCacheAndCacheTrait for cache management

---
+++ src/Traits/Repository/EncryptCacheAndCacheTrait.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/03/2025, 21:30
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    EncryptCacheAndCacheTrait.php
+ * @date    21/03/2025
+ * @time    21:29
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Traits\Repository;
+
+use Idm\Bundle\Settings\Interfaces\Entity\UseEncryptCacheInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+trait EncryptCacheAndCacheTrait
+{
+	private CacheItemPoolInterface&TagAwareCacheInterface $cacheEncrypt;
+	private CacheItemPoolInterface&TagAwareCacheInterface $cache;
+
+	public function getCache (?bool $encrypted = null): CacheItemPoolInterface&TagAwareCacheInterface
+	{
+		$encrypted = $encrypted ?? is_subclass_of($this->getEntityName(), UseEncryptCacheInterface::class);
+
+		return $encrypted ? $this->cacheEncrypt : $this->cache;
+	}
+
+	public function setCache (CacheItemPoolInterface&TagAwareCacheInterface $cache): self
+	{
+		$this->cache = $cache;
+
+		return $this;
+	}
+
+	public function setCacheEncrypt (CacheItemPoolInterface&TagAwareCacheInterface $cacheEncrypt): self
+	{
+		$this->cacheEncrypt = $cacheEncrypt;
+
+		return $this;
+	}
+}
