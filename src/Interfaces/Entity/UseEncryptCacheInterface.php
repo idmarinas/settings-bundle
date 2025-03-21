8a8c3e4a2714246f8d428f844fdab7276cb3895c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-21 21:47:39 +0100

    feat(interface): add UseEncryptCacheInterface for encryption caching

---
+++ src/Interfaces/Entity/UseEncryptCacheInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 21/03/2025, 21:32
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    UseEncryptCacheInterface.php
+ * @date    21/03/2025
+ * @time    21:25
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Interfaces\Entity;
+
+interface UseEncryptCacheInterface {}
