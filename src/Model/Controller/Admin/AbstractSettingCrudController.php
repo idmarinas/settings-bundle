a461ab50fa527f707330ba51feedf3df5431921c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-24 18:49:33 +0100

    feat(controller): add abstract setting domain and setting CRUD controllers

---
+++ src/Model/Controller/Admin/AbstractSettingCrudController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 23/03/2025, 19:38
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    AbstractSettingCrudController.php
+ * @date    22/03/2025
+ * @time    11:21
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Model\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\SearchMode;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Idm\Bundle\Settings\Model\Controller\Admin\Traits\TranslatableFieldsTrait;
+use function Symfony\Component\Translation\t;
+
+abstract class AbstractSettingCrudController extends AbstractCrudController
+{
+	use TranslatableFieldsTrait;
+
+	public function configureCrud (Crud $crud): Crud
+	{
+		$t = fn(string $message) => t($message, [], 'IdmSettingsBundle');
+
+		return parent::configureCrud($crud)
+			->setEntityLabelInSingular($t('entity.label.setting.singular'))
+			->setEntityLabelInPlural($t('entity.label.setting.plural'))
+			->setSearchFields(['name', 'description'])
+			->setSearchMode(SearchMode::ANY_TERMS)
+		;
+	}
+
+	public function configureFilters (Filters $filters): Filters
+	{
+		return parent::configureFilters($filters)
+			->add('name')
+			->add('description')
+			->add('slug')
+		;
+	}
+
+	public function configureFields (string $pageName): iterable
+	{
+		$t = fn(string $message) => t($message, [], 'IdmSettingsBundle');
+
+		yield from $this->translatableFields();
+
+		yield FormField::addTab($t('crud.form.tab.info'), 'fa fa-info');
+		yield IdField::new('id', $t('entity.common.id'))->onlyOnDetail();
+		yield TextField::new('name', $t('entity.common.name'));
+		yield TextareaField::new('description', $t('entity.common.description'))->hideOnIndex();
+		yield AssociationField::new('domain', $t('entity.setting.domain'));
+		yield IntegerField::new('priorityOrder', $t('entity.common.priority_order'));
+		yield TextField::new('slug', $t('entity.common.slug'))->onlyOnDetail();
+	}
+}
---
+++ src/Model/Controller/Admin/AbstractSettingDomainCrudController.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 23/03/2025, 19:38
+ *
+ * @project IDMarinas Settings Bundle
+ * @see     https://github.com/idmarinas/settings-bundle
+ *
+ * @file    AbstractSettingDomainCrudController.php
+ * @date    22/03/2025
+ * @time    11:21
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Settings\Model\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\SearchMode;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Idm\Bundle\Settings\Model\Controller\Admin\Traits\TranslatableFieldsTrait;
+use function Symfony\Component\Translation\t;
+
+abstract class AbstractSettingDomainCrudController extends AbstractCrudController
+{
+	use TranslatableFieldsTrait;
+
+	public function configureCrud (Crud $crud): Crud
+	{
+		$t = fn(string $message) => t($message, [], 'IdmSettingsBundle');
+
+		return parent::configureCrud($crud)
+			->setEntityLabelInSingular($t('entity.label.setting.singular'))
+			->setEntityLabelInPlural($t('entity.label.setting.plural'))
+			->setSearchFields(['name', 'description'])
+			->setSearchMode(SearchMode::ANY_TERMS)
+		;
+	}
+
+	public function configureFilters (Filters $filters): Filters
+	{
+		return parent::configureFilters($filters)
+			->add('name')
+			->add('description')
+			->add('slug')
+		;
+	}
+
+	public function configureFields (string $pageName): iterable
+	{
+		$t = fn(string $message) => t($message, [], 'IdmSettingsBundle');
+
+		yield from $this->translatableFields();
+
+		yield FormField::addTab($t('crud.form.tab.info'), 'fa fa-info');
+		yield IdField::new('id', $t('entity.common.id'))->onlyOnDetail();
+		yield BooleanField::new('enabled', $t('entity.common.enabled'));
+		yield BooleanField::new('readOnly', $t('entity.common.read_only'));
+		yield TextField::new('name', $t('entity.common.name'));
+		yield TextareaField::new('description', $t('entity.common.description'))->hideOnIndex();
+		yield IntegerField::new('priorityOrder', $t('entity.common.priority_order'));
+		yield TextField::new('slug', $t('entity.common.slug'))->onlyOnDetail();
+	}
+}
