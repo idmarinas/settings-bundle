eeffe38b424e4f8fd23e3c8209b39cd64366f5ce
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-31 17:28:15 +0200

    feat(controller): enhance AbstractSettingCrudController with new fields
    - Added ChoiceField for 'type' with translatable choices.
    - Updated yield statements for better readability and structure.
    - Improved organization of fields in the form.

--- src/Model/Controller/Admin/AbstractSettingCrudController.php
+++ src/Model/Controller/Admin/AbstractSettingCrudController.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 27/03/2025, 18:32
+ * Last modified by "IDMarinas" on 31/03/2025, 17:20
  *
  * @project IDMarinas Settings Bundle
  * @see     https://github.com/idmarinas/settings-bundle
@@ -19,17 +19,19 @@
 
 namespace Idm\Bundle\Settings\Model\Controller\Admin;
 
+use Admin\Util\EnumUtils;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Option\SearchMode;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Idm\Bundle\Settings\Enums\SettingsEnum;
 use function Symfony\Component\Translation\t;
 
 abstract class AbstractSettingCrudController extends AbstractCrudController
@@ -59,18 +61,26 @@ abstract class AbstractSettingCrudController extends AbstractCrudController
 	{
 		$t = fn(string $message) => t($message, [], 'IdmSettingsBundle');
 
-		yield FormField::addTab($t('crud.form.tab.translation'), 'fa fa-language');
-		yield BooleanField::new('translatable', $t('crud.common.translatable.label'))
+		yield 'translatable' => BooleanField::new('translatable', $t('crud.common.translatable.label'))
 			->setHelp($t('crud.common.translatable.help'))
 		;
-		yield TextField::new('translationDomain', $t('crud.common.translation_domain'))->hideOnIndex();
+		yield 'translation_domain' => TextField::new('translationDomain', $t('crud.common.translation_domain'))
+			->hideOnIndex()
+		;
+		yield 'id' => IdField::new('id', $t('crud.common.id'))->onlyOnDetail();
+		yield 'name' => TextField::new('name', $t('crud.common.name'));
+		yield 'description' => TextareaField::new('description', $t('crud.common.description'))->hideOnIndex();
+		yield 'domain' => AssociationField::new('domain', $t('crud.setting.domain'))->autocomplete();
+		yield 'priority_order' => IntegerField::new('priorityOrder', $t('crud.common.priority_order'));
+		yield 'slug' => TextField::new('slug', $t('crud.common.slug'))->onlyOnDetail();
 
-		yield FormField::addTab($t('crud.form.tab.info'), 'fa fa-info');
-		yield IdField::new('id', $t('crud.common.id'))->onlyOnDetail();
-		yield TextField::new('name', $t('crud.common.name'));
-		yield TextareaField::new('description', $t('crud.common.description'))->hideOnIndex();
-		yield 'domain' => AssociationField::new('domain', $t('crud.setting.domain'));
-		yield IntegerField::new('priorityOrder', $t('crud.common.priority_order'));
-		yield TextField::new('slug', $t('crud.common.slug'))->onlyOnDetail();
+		yield 'type' => ChoiceField::new('type', $t('crud.setting.type'))
+			->renderAsBadges()
+			->setFormTypeOption('choice_value', fn($value) => is_string($value) ? $value : $value->value)
+			->setTranslatableChoices(
+				SettingsEnum::toTranslatableChoices('enum.setting.type.', domain: 'IdmSettingsBundle')
+			)
+		;
+		yield 'value' => TextField::new('value', $t('crud.common.value'))->hideOnIndex();
 	}
 }
